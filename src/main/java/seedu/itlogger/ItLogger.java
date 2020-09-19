75967ed0f87abb61582485b4d302c2e8a27a1196
DESKTOP-K455E2E\hongj <e0264456@u.nus.edu>
2020-09-19 15:19:10 +0800

    Edit coding patterns to follow gradle rules

--- src/main/java/seedu/itlogger/ItLogger.java
+++ src/main/java/seedu/itlogger/ItLogger.java
@@ -26,7 +26,7 @@ public class ItLogger {
 
         printLogo();
         askName();
-        String userName="";
+        String userName = "";
         if (hasNextLine()) {
             userName = getInput();
         }
