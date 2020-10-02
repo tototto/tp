6d9b7bbf2ef46a8b0229b8b8e2e164fe2002aea1
Januarius Jang <>
2020-10-02 23:25:58 +0800

    removed unused changes

--- src/test/java/seedu/itlogger/ItLoggerTest.java
+++ src/test/java/seedu/itlogger/ItLoggerTest.java
@@ -1,8 +1,6 @@
 package seedu.itlogger;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static seedu.itlogger.Parser.*;
 
 
 import org.junit.jupiter.api.Test;
